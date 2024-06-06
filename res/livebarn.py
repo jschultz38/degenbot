5bd5585c51ebb4f077d408873e7def8c174cbe35
jeremyf <jeremyf@zillowgroup.com>
2024-06-06 12:19:27 -0700

    Creating livebarn file to establish IDs for rinks and build URLs

---
+++ res/livebarn.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class Livebarn(Enum)
+    KENT = 1276
+    AOTW = 1686
+    EVCOMM = 1687
+    VMFH = 2272
+    STAR = 2270
+    SMART = 2271
+    LIC = 893
+    OVA = 761
+    RENTON_SMALL = 1048
+    RENTON = 1047
+    SNOQUALMIE_A = 1533
+    SNOQUALMIE_B = 1532
+
+
+
