a1c19155332b16d19c9ac84d5f27d6a1bd5f942b
Jarrett Schultz <jschultz38@gatech.edu>
2024-06-06 19:19:57 -0700

    Standardize HockeyGame string repr
    Standardize HockeyGame string representation. Instead of asking for a string
    representation from the user, ask for the data and create the representation
    inside the class
    
    Signed-off-by: Jarrett Schultz <jschultz38@gatech.edu>

--- game.py
+++ game.py
@@ -1,8 +1,71 @@
+# previous
+# Tue, May 14, 9:20 PM, KCI VMFH, AWAY, W, 9 - 1, ^BSOD vs Monarchs 5D
+#
+# upcoming
+# Thu, Jun 13, 8:00 PM, KVIC, HOME, ^BSOD vs Highlanders
+
+# What do I need?
+# - datetime of the game, rink, teams, side of teams, side of degen
+# + (for completed games) score with side of team
+
+# - datetime of the game
+# - rink
+# - home team
+# - away team
+# - degen team
+###############
+# - home score
+# - away score
+
 class HockeyGame:
-    def __init__(self, team, textual_rep, gametime):
+    # Macros used for dictating which team the degen is on
+    DEGEN_HOME = 0
+    DEGEN_AWAY = 1
+
+    def __init__(self, team, datetime_obj, location, home_team, away_team, degen_team, home_score=None, away_score=None):
+        # this is the json object of the team - should look the same as in res/teams.json
         self.team = team
-        self.text = textual_rep
-        self.gametime = gametime
+        self.gametime = datetime_obj
+        self.location = location
+        self.home_team = home_team
+        self.away_team = away_team
+        # This value should be one of the above macros
+        self.degen_team = degen_team
+        self.home_score = home_score
+        self.away_score = away_score
+
+    def to_string(self):
+        is_degen_home = (self.degen_team == self.DEGEN_HOME)
+        side = 'HOME' if is_degen_home else 'AWAY'
+
+        string_repr = self.gametime.strftime("%A, %B %d, %I:%M %p") + ", " + \
+                        self.location + ", " + \
+                        side + ", "
+
+        # Add in result if there is one
+        if self.home_score != None:
+            result = None
+            if self.home_score == self.away_score:
+                result = 'T'
+            elif (is_degen_home and self.home_score > self.away_score) or (not is_degen_home and self.home_score < self.away_score):
+                result = 'W'
+            else:
+                result = 'L'
+
+            if is_degen_home:
+                string_repr += result + ", " + \
+                                str(self.home_score) + " - " + str(self.away_score) + ", "
+            else:
+                string_repr += result + ", " + \
+                                str(self.away_score) + " - " + str(self.home_score) + ", "
+
+        # Add team names, with ^ marking the degen team
+        if is_degen_home:
+            string_repr += "^" + self.home_team + " vs " + self.away_team
+        else:
+            string_repr += "^" + self.away_team + " vs " + self.home_team
+
+        return string_repr
 
     def __str__(self):
-        return self.text
+        return self.to_string()
