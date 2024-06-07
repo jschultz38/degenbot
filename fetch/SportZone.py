d83f44419dd9e9e0bcb8490b1e4e79df597c6379
Jarrett Schultz <jschultz38@gatech.edu>
2024-06-07 08:16:44 -0700

    Add new HockeyGame format
    Change the sportzsone file to use the new HockeyGame
    format.
    
    Signed-off-by: Jarrett Schultz <jschultz38@gatech.edu>

--- fetch/SportZone.py
+++ fetch/SportZone.py
@@ -1,20 +1,40 @@
 import datetime
 from game import HockeyGame
 
-
-# Create game object for sportzone backed websites (KHL, Pride)
 def createSportZoneGame(cols, team):
-    # Create textual representation
+    # Get the easy stuff
+    location = cols[3].getText()
+    degen_team = team['name']
+    side = cols[4].getText()
+    is_degen_home = (side == "HOME")
+
+    # Add result-dependant variables
     cols_text = None
+    opponent_team = None
+    away_score = None
+    home_score = None
     if ("Preview" in cols[-1].getText()):
-        opponent = cols[-4].getText()
-        cols_text = ", ".join(map(lambda e: e.getText(), cols[1:-4:]))
-        cols_text += ", ^" + team['name'] + " vs " + opponent
+        opponent_team = cols[-4].getText()
     else:
-        opponent = cols.pop(5).getText()
-        cols_text = ", ".join(map(lambda e: e.getText(), cols[1:-1:]))
-        cols_text += ", ^" + team['name'] + " vs " + opponent
-    textual_rep = cols_text
+        opponent_team = cols[5].getText()
+        score_text = cols[7].getText().split(" ")
+        degen_score = int(score_text[0])
+        opponent_score = int(score_text[2])
+
+        if is_degen_home:
+            home_score = degen_score
+            away_score = opponent_score
+        else:
+            home_score = opponent_score
+            away_score = degen_score
+
+    # Place teams on home/away
+    if is_degen_home:
+        home_team = degen_team
+        away_team = opponent_team
+    else:
+        home_team = opponent_team
+        away_team = degen_team
 
     # Find game time
     dateText = cols[1].getText()
@@ -43,8 +63,7 @@ def createSportZoneGame(cols, team):
         case _:
             print("ERROR: Could not decode: " + month_text)
             month_ret = 1
-    month_ret = int(month_ret)
-
+        
     ## Get time
     hour = timeText.split(":")[0]
     minute = timeText.split(":")[1].split(" ")[0]
@@ -53,9 +72,18 @@ def createSportZoneGame(cols, team):
     hour_ret = int(hour) if meridiem == "AM" else int(hour) + 12
     minute_ret = int(minute)
 
-    gametime = datetime.datetime(2024, int(month_ret), int(day_ret), hour=hour_ret, minute=minute_ret)
-
-    game = HockeyGame(team, textual_rep, gametime)
-    print(game)
+    gametime = datetime.datetime(2024, month_ret, day_ret, hour=hour_ret, minute=minute_ret)
+    
+    # Create the game
+    game = HockeyGame(
+                team,
+                gametime,
+                location,
+                home_team,
+                away_team,
+                HockeyGame.DEGEN_HOME if is_degen_home else HockeyGame.DEGEN_AWAY,
+                home_score=home_score,
+                away_score=away_score
+                )
 
     return game
