580ccaf32f9151264fb76d799caae2d2f173754f
Jarrett Schultz <jschultz38@gatech.edu>
2024-06-14 11:20:58 -0700

    Add !sus
    Add !sus which returns all suspensions for a player.
    
    In order to get this to work in a reasonable amount of time,
    I had to cache all previous suspensions which right now is a
    manual process using saveAllKHLSeasons.py. Every time a new
    season is created, this would have to be updated. If I really care,
    I would make a way to automatically detect on startup if a new season
    has been created. So mark that one up in the issues column
    
    Signed-off-by: Jarrett Schultz <jschultz38@gatech.edu>

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import datetime
+from utils.player import Suspension
 
 from bot import createBasicBot
 from globals import *
@@ -6,14 +8,32 @@ from credentials import prod_token, test_token
 
 def main():
     # Read in teams
+    print("reading in json...", end ="")
+
     json_data = None
     with open("res/teams.json") as teams_file:
         json_data = json.load(teams_file)
+    teams = json_data["teams"]
 
-    TEAMS = json_data["teams"]
+    with open("res/seasons.json") as teams_file:
+        json_data = json.load(teams_file)
+    seasons = json_data
+
+    print('done')
+
+    # Load seasons cache into memory
+    print('loading past suspensions...', end ="")
+
+    suss = []
+    with open("res/past_seasons_cache.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            suss.append(eval(line))
+    seasons['khl']['past_seasons_cache'] = suss
+
+    print('done')
 
     # Set up bot
-    bot = createBasicBot(TEAMS)
+    bot = createBasicBot([teams, seasons])
     bot.run(test_token if USE_TEST_TOKEN else prod_token)
 
 if __name__ == '__main__':
