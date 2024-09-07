77033e7273c3bbc9fb3ed93ba3de50a3685fad1f
Jarrett Schultz <jschultz38@gatech.edu>
2024-09-06 18:09:37 -0700

    add some helpful descriptions
    Signed-off-by: Jarrett Schultz <jschultz38@gatech.edu>

--- globals.py
+++ globals.py
@@ -1,7 +1,17 @@
 from threading import Lock
 
+'''Uses alternate token named 'test_token' in credentials.py'''
 USE_TEST_TOKEN = False
-TEST_MODE = False
+
+'''True to spin up caching thread'''
 USE_CACHING = True
+
+'''Puts bot in test mode, which doesn't make any http requests and only uses a
+saved .html file for a single team for player Jarrett. Only really useful if you
+are doing an overhaul to the bot infrastructure and need to iterate quickly
+without bothering with http requests.'''
+TEST_MODE = False
+
+'''Don't touch these'''
 CACHING_LOCK = Lock()
 SEASONS = []
