b42a54fd235d114731ab36f889ec55fa15407818
Jarrett Schultz <jschultz38@gatech.edu>
2024-09-06 18:19:33 -0700

    add the rest of the months to translateMonth
    Signed-off-by: Jarrett Schultz <jschultz38@gatech.edu>

--- utils/common.py
+++ utils/common.py
@@ -1,6 +1,12 @@
 def translateMonth(month_text):
 	month_ret = None
 	match month_text:
+		case 'January' | 'Jan':
+			month_ret = 1
+		case 'February' | 'Feb':
+			month_ret = 2
+		case 'March' | 'Mar':
+			month_ret = 3
 		case 'April' | 'Apr':
 			month_ret = 4
 		case 'May':
@@ -11,8 +17,14 @@ def translateMonth(month_text):
 			month_ret = 7
 		case 'August' | 'Aug':
 			month_ret = 8
-		case 'Sep':
+		case 'September' | 'Sep':
 			month_ret = 9
+		case 'October' | 'Oct':
+			month_ret = 10
+		case 'November' | 'Nov':
+			month_ret = 11
+		case 'December' | 'Dec':
+			month_ret = 12
 		case _:
 			print("ERROR: Could not decode: " + month_text)
 			month_ret = 1
