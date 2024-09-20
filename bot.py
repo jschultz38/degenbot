76c1a7f5a2563ea50a33cf7aa67dc7c2b5cdba27
Jarrett Schultz <jschultz38@gatech.edu>
2024-09-20 11:49:31 -0700

    Enhance error handling
    Signed-off-by: Jarrett Schultz <jschultz38@gatech.edu>

--- bot.py
+++ bot.py
@@ -37,10 +37,19 @@ def createBasicBot(teams):
         print("time to respond is: " + str(datetime.now() - ctx.extras['before_time']))
 
     async def on_error(ctx, error):
+        handled = False
+
         if isinstance(error, commands.UserInputError):
             await ctx.send("I cant understand you when you use ' or \"")
+            handled = True
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("Command not found")
+            handled = True
+
+        if handled:
             print("handled error: " + str(error))
             return
+
         raise error
     bot.on_command_error = on_error
 
